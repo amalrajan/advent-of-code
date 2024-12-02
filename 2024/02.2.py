a30b0d9c55686ed4107ecb3974701e5ef560bccc
Amal Rajan <amalrajan74@gmail.com>
2024-12-02 10:20:55 -0500

    Refactor safety checks in is_safe and is_safe_by_removing_one functions

--- 2024/02.2.py
+++ 2024/02.2.py
@@ -15,6 +15,8 @@ def parse_input():
 
 
 def is_safe_by_removing_one(nums):
+    # Time complexity: O(N)
+    # If I were to simulate removing elements one by one, it would cost O(N^2)
     def left_to_right():
         prev = nums[0] - 1
         flag = False
@@ -50,36 +52,31 @@ def is_safe(nums):
     increasing = False
     decreasing = False
 
-    is_valid = True
-
     for i in range(1, len(nums)):
         diff = nums[i] - nums[i - 1]
 
-        if diff >= 0:
+        if not 1 <= abs(diff) <= 3:
+            return False
+
+        if diff > 0:
             increasing = True
-        else:
+        elif diff < 0:
             decreasing = True
 
         if increasing and decreasing:
-            is_valid = False
-            break
+            return False
 
-        if not 1 <= abs(diff) <= 3:
-            is_valid = False
-            break
-
-    return is_valid
+    return True
 
 
 def solve(reports):
     ans = 0
 
     for report in reports:
-        if is_safe(report):
+        if is_safe(report) or (
+            is_safe_by_removing_one(report) or is_safe_by_removing_one(report[::-1])
+        ):
             ans += 1
-        else:
-            if is_safe_by_removing_one(report) or is_safe_by_removing_one(report[::-1]):
-                ans += 1
 
     return ans
 
